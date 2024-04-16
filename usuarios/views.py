8dc3d6a1b312ad603e5b344be8290ed26223ee3a
Jadson José da Silva <jadsonjosesilva@gmail.com>
2024-04-15 22:00:32 -0300

    sinc

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def cadastro(request):
-    return HttpResponse('Ola Mundo!')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        return HttpResponse('Teste')
