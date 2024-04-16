335f646187a2be01bf09aed4351dd68c7c07787e
Jadson José da Silva <jadsonjosesilva@gmail.com>
2024-04-16 12:12:00 -0300

    Cont

--- healing/settings.py
+++ healing/settings.py
@@ -116,3 +116,14 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 # Create your views here.
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
     elif request.method == "POST":
-        return HttpResponse('Teste')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, "A senha e o confirmar senha devem ser iguais.")
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, "A senha deve ter mais que 6 digitos.")        
+            return redirect('/usuarios/cadastro')
+        
+        users = User.objects.filter(username=username)
+        print(users.exists())
+        
+        if users.exists():
+            messages.add_message(request, constants.ERROR, "Já existe um usuário com esse nome.")
+            return redirect('/usuarios/cadastro')
+        
+        
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=senha
+        )
+        
+        return redirect('/usuarios/login')
